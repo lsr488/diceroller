0b2bf17706bc1d1fa8aef23b174622b332ad3f2e
Lisa <lsr488@gmail.com>
2018-07-13 21:07:26 -0400

    Displays how often a number was rolled.

--- diceroll.js
+++ diceroll.js
@@ -1,30 +1,17 @@
-// input
-	// the number of dice to roll
-	// how many sides each die has
-// ouput
-	// the result of the rolls, as an integer
-		// Math.rand that's within range of the number of die sides
-		// ex 2d6 that rolled a 2 and 4 = 6
-
-// 1d6
-
-// Math.random() & Math.floor()
-
 // number of dice to roll
-var numberOfDice = 6;
+// var numberOfDice = 5;
 // number of sides each dice has
-var sides = 6;
+// var sides = 12;
+// 2D array to organize rolls by #
+var orgRolls = []
 
-
-function rollDice() {
+function rollDice(numberOfDice, sides) {
 	var results = [];
 
-	// for each numberOfDice
+	// roll dice (get a number between 1 and max number of die-sides)
+	// store the value in the results array	
 	for(var i = 0; i < numberOfDice; i++) {
-		// roll dice (get a number between 1 and max number of die-sides)
-		// store the value in the results array	
 		results.push(Math.floor(Math.random() * sides + 1));		
-		// console.log(results);
 	}
 
 	// add all side results together
@@ -32,137 +19,45 @@ function rollDice() {
 		return accumulator + currentValue;
 	});
 
+	// display stats, rolls, and sum of rolls
 	console.log("Num Dice:", numberOfDice);
 	console.log("Sides:", sides);
 	console.log(sum + ": " + results.join(', '));
 	console.log("=====================");
 
-	createRes();
-	// checkRolls1(results);
-	checkRolls(results);
+	organizeRolls(sides);
+	sortRolls(results);
 	displayNumbers();
-
 }
 
-// create an object 
-	// key == number of side
-
-var res = []
-
-function createRes() {
+function organizeRolls(sides) {
 	for(var i = 0; i < sides; i++) {
-		res[i] = [];
+		orgRolls[i] = [];
 	}
 }
 
-
-function checkRolls(results) {
-	// for each result
-		// sort into appropriate res array
-	
-	for(var i = 0; i < sides; i++) {
-		results.find(function(num) {
-			if(num - 1 == i) {
-				res[i].push(num);
-			}
-		});
-	}
-		console.log(res);
+function sortRolls(results) {
+	results.forEach(function(element) {
+		var index = element - 1;
+		orgRolls[index].push(element);
+	});
 }
 
-function checkRolls1(results) {
-	// var res1 = [];
-	// var res2 = [];
-	// var res3 = [];
-	// var res4 = [];
-	// var res5 = [];
-	// var res6 = [];
-
-	results.find(function(num) {
-		if(num == 1) {
-			// res1.push(num);
-			res[0].push(num);
-		}
-	});
-
-	results.find(function(num) {
-		if(num == 2) {
-			// res2.push(num);
-			res[1].push(num);
-		}
-	});
-
-	results.find(function(num) {
-		if(num == 3) {
-			// res3.push(num);
-			res[2].push(num);
-		}
-	});
-
-	results.find(function(num) {
-		if(num == 4) {
-			// res4.push(num);
-			res[3].push(num);
-		}
-	});
-
-	results.find(function(num) {
-		if(num == 5) {
-			// res5.push(num);
-			res[4].push(num);
-		}
-	});
-
-	results.find(function(num) {
-		if(num == 6) {
-			// res6.push(num);
-			res[5].push(num);
-		}
-	});
-
-	// var prob = (1/(numberOfDice * sides) * 100);
-	// console.log("Prob: " + prob + "%");
-
-	// console.log("# of 1s:", res1.length);
-	// console.log((res1.length/results.length)*100 + "%");
-	
-	// console.log("# of 2s:", res2.length);
-	// console.log((res2.length/results.length)*100 + "%");
-	
-	// console.log("# of 3s:", res3.length);
-	// console.log((res3.length/results.length)*100 + "%");
-	
-	// console.log("# of 4s:", res4.length);
-	// console.log((res4.length/results.length)*100 + "%");
-	
-	// console.log("# of 5s:", res5.length);
-	// console.log((res5.length/results.length)*100 + "%");
-	
-	// console.log("# of 6s:", res6.length);
-	// console.log((res6.length/results.length)*100 + "%");
-	
-	// console.log(res);
-}
-
-// TODO: make display adjustable for more than 6 sides
 function displayNumbers() {
-	// res.forEach(function(disp) {
-	// 	console.log("# of " + disp + ": ");
-	// });
-
-	console.log("# of 1s:", res[0].length);
-	console.log("# of 2s:", res[1].length);
-	console.log("# of 3s:", res[2].length);
-	console.log("# of 4s:", res[3].length);
-	console.log("# of 5s:", res[4].length);
-	console.log("# of 6s:", res[5].length);
+	for(var i = 0; i < orgRolls.length; i++) {
+		if(orgRolls[i].length > 0) {
+			console.log("# of " + orgRolls[i][0] + "s: " + orgRolls[i].length);
+		}
+	}
 }
 
-rollDice();
+// rollDice();
+
+// TODO: make it accept interactive input
 
 // rollDice(5, 12);
 // rollDice(6, 4);
 // rollDice(1, 2);
-// rollDice(3, 6);
+rollDice(3, 6);
 // rollDice(4, 20);
 // rollDice(100, 100);
